ef46c0747a067b20f48063605021ef27838b9c3b
Saul Hardman <hello@iamsaul.co.uk>
2016-05-05 12:27:34 +0100

    fix(Tracking): render iframe directly and load YouGov tracking via `loadScript`

--- src/components/tracking/tracking.js
+++ src/components/tracking/tracking.js
@@ -1,7 +1,18 @@
+import loadScript from '../../js/lib/loadScript';
 import Component from '../../js/modules/component';
 import Templates from '../../js/templates';
 
-class Tracking extends Component {}
+const YOU_GOV_TRACKING_URL = '//tracker.yougov.com/campaign/11?name=inter';
+
+class Tracking extends Component {
+    init() {
+        this.config.randomNumber = Math.random() * 10000000000000;
+
+        loadScript(YOU_GOV_TRACKING_URL);
+
+        return this.render();
+    }
+}
 
 Tracking.prototype.template = Templates.tracking;
 
