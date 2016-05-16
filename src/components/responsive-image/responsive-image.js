7be99771c363fe00af3882e0142e267b0a5fa648
Saul Hardman <hello@iamsaul.co.uk>
2016-05-16 13:23:18 +0100

    feat(Responsive Image): add responsive image component

---
+++ src/components/responsive-image/responsive-image.js
@@ -0,0 +1,28 @@
+import template from './responsive-image.html!text';
+import Vue from 'vue';
+
+let ResponsiveImage = Vue.extend({
+    template,
+    props: {
+        path: String,
+        extension: {
+            type: String,
+            default: '.jpg'
+        },
+        widths: {
+            type: Array,
+            default: [140, 500, 1000, 2000, 5760]
+        },
+        sizes: {
+            default: '100vw'
+        },
+        alt: String
+    },
+    computed: {
+        srcSet() {
+            return this.widths.map((width) => `${ this.path }/${ width }${ this.extension } ${ width }w`).join(', ');
+        }
+    }
+});
+
+export default ResponsiveImage;
