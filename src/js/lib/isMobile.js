8fdfa194f47eeef5b6f7e6904f092fcbb5168913
Saul Hardman <hello@iamsaul.co.uk>
2016-05-17 19:21:51 +0100

    feat(Utils): update `MOBILE_THRESHOLD` to be inline with CSS breakpoints

--- src/js/lib/isMobile.js
+++ src/js/lib/isMobile.js
@@ -1,6 +1,6 @@
 import bowser from 'ded/bowser';
 
-const MOBILE_THRESHOLD = 600;
+const MOBILE_THRESHOLD = 740;
 
 export default function isMobile() {
     return (bowser.mobile || bowser.tablet || document.body.clientWidth < MOBILE_THRESHOLD) ? true : false;
