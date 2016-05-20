427e988d9049e51875d467da0f96102463637ee4
Saul Hardman <hello@iamsaul.co.uk>
2016-05-20 17:49:39 +0100

    feat(Subhead): add variant of subhead that has video/image background

--- src/components/subhead/subhead.js
+++ src/components/subhead/subhead.js
@@ -1,8 +1,14 @@
 import Block from '../../js/modules/Block';
+import ResponsiveImage from '../responsive-image/responsive-image';
 import template from './subhead.html!text';
+import Video from '../video/video';
 
 let Subhead = Block.extend({
-    template
+    template,
+    components: {
+        ResponsiveImage,
+        Video
+    }
 });
 
 export default Subhead;
