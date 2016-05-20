41fa1f3de41d146af0e8f1dfb7e3137ac38977c9
Saul Hardman <hello@iamsaul.co.uk>
2016-05-20 10:17:35 +0100

    feat(Interactive): add new illustrations and amend content

--- src/components/interactive-task/interactive-task.js
+++ src/components/interactive-task/interactive-task.js
@@ -5,10 +5,8 @@ import template from './interactive-task.html!text';
 // Components
 import Button from '../../components/button/button';
 
-// Place setting partials
-import placeSettingNormal from '../../partials/place-setting-normal.svg!text';
-import placeSettingHeavy from '../../partials/place-setting-heavy.svg!text';
-import placeSettingLight from '../../partials/place-setting-light.svg!text';
+// Task partials
+import task1ShapeAngular from '../../partials/task-1-shape-angular.svg!text';import task1ShapeDefault from '../../partials/task-1-shape-default.svg!text';import task1ShapeRounded from '../../partials/task-1-shape-rounded.svg!text';import task1SmellAdd from '../../partials/task-1-smell-add.svg!text';import task1SmellDefault from '../../partials/task-1-smell-default.svg!text';import task1SmellNone from '../../partials/task-1-smell-none.svg!text';import task2WeightDefault from '../../partials/task-2-weight-default.svg!text';import task2WeightHeavy from '../../partials/task-2-weight-heavy.svg!text';import task2WeightLight from '../../partials/task-2-weight-light.svg!text';import task2SoundClassical from '../../partials/task-2-sound-classical.svg!text';import task2SoundDefault from '../../partials/task-2-sound-default.svg!text';import task2SoundNone from '../../partials/task-2-sound-none.svg!text';import task3ColourDefault from '../../partials/task-3-colour-default.svg!text';import task3ColourMono from '../../partials/task-3-colour-mono.svg!text';import task3ColourMulti from '../../partials/task-3-colour-multi.svg!text';import task3SoundDefault from '../../partials/task-3-sound-default.svg!text';import task3SoundLoud from '../../partials/task-3-sound-loud.svg!text';import task3SoundLow from '../../partials/task-3-sound-low.svg!text';
 
 // Patron partials
 import patron1Neutral from '../../partials/patron-1-neutral.svg!text';
@@ -27,9 +25,24 @@ let InteractiveTask = Vue.extend({
         Button
     },
     partials: {
-        placeSettingNormal,
-        placeSettingHeavy,
-        placeSettingLight,
+        task1ShapeAngular,
+        task1ShapeDefault,
+        task1ShapeRounded,
+        task1SmellAdd,
+        task1SmellDefault,
+        task1SmellNone,
+        task2WeightDefault,
+        task2WeightHeavy,
+        task2WeightLight,
+        task2SoundClassical,
+        task2SoundDefault,
+        task2SoundNone,
+        task3ColourDefault,
+        task3ColourMono,
+        task3ColourMulti,
+        task3SoundDefault,
+        task3SoundLoud,
+        task3SoundLow,
         patron1Neutral,
         patron1Happy,
         patron1Sad,
@@ -89,10 +102,10 @@ let InteractiveTask = Vue.extend({
     computed: {
         placeSettingPartialName() {
             if (typeof this.selectedOptionId !== 'undefined') {
-                return `place-setting-${ this.selectedOptionId }`;
+                return `task-${ this.challengeId }-${ this.task.id }-${ this.selectedOptionId }`;
             }
 
-            return 'place-setting-normal';
+            return `task-${ this.challengeId }-${ this.task.id }-default`;
         },
         patronPartialName() {
             if (this.isAnswered) {
--- src/js/modules/Challenges.js
+++ src/js/modules/Challenges.js
@@ -5,17 +5,18 @@ const CHALLENGES = {
         title: 'This sweet toothed explorer avoids bland food at all cost',
         tasks: [
             {
+                id: 'shape',
                 title: 'Can you sweeten a bland deal?',
                 description: 'How would you use shape to make this dish seem sweeter?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'rounded',
                         title: 'Serve more rounded food',
                         response: 'Correct! Studies show that food in round shapes enhance the perception of sweetness.',
                         isCorrect: true
                     },
                     {
-                        id: 'light',
+                        id: 'angular',
                         title: 'Serve more angular food',
                         response: 'Sorry – science suggests that we perceive square-ish foods to be more tart.',
                         isCorrect: false
@@ -23,17 +24,18 @@ const CHALLENGES = {
                 ]
             },
             {
+                id: 'smell',
                 title: 'Can you sweeten the deal?',
                 description: 'How would you like to use aroma?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'add',
                         title: 'Introduce the scent of vanilla to the dish',
                         response: 'Correct! The scent of vanilla CAN make foods seem sweeter.',
                         isCorrect: true
                     },
                     {
-                        id: 'light',
+                        id: 'none',
                         title: 'Add nothing – this dish already smells great as it is',
                         response: 'Sorry! Doing nothing won\'t change the taste of the food, but you’ve missed an opportunity to enhance the sweetness.',
                         isCorrect: false
@@ -46,17 +48,18 @@ const CHALLENGES = {
         title: 'The fine diner who will fork out for a great meal – if it’s worth it',
         tasks: [
             {
+                id: 'weight',
                 title: 'What’s going to make this a classy night to remember?',
                 description: 'How would you serve your classy?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'light',
                         title: 'Keep it classy: time to bring out the fine China',
                         response: 'Sorry! Though fancy-looking, current science suggests that it’s heavier wares that make a meal feel more expensive.',
                         isCorrect: false
                     },
                     {
-                        id: 'light',
+                        id: 'heavy',
                         title: 'Beef it up with heavy cutlery and sturdy crockery',
                         response: 'Correct! Hefty bowls and weighty cutlery add an air of quality to a meal.',
                         isCorrect: true
@@ -64,17 +67,18 @@ const CHALLENGES = {
                 ]
             },
             {
+                id: 'sound',
                 title: 'What’s going to make this a classy night to remember?',
                 description: 'Which sounds like it would be more expensive?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'classical',
                         title: 'Hire a string quartet to bust out some Brahms',
                         response: 'Correct! It probably comes as no surprise that classical music is often perceived as the marker of a quality experience.',
                         isCorrect: true
                     },
                     {
-                        id: 'light',
+                        id: 'none',
                         title: 'Keep it silent: let people enjoy their conversations for once ',
                         response: 'Sorry! Silence can make a room feel empty – music is one of the most powerful dining cues we have.',
                         isCorrect: false
@@ -87,17 +91,18 @@ const CHALLENGES = {
         title: 'A grey panther in danger of eating too little… and adding too much salt when he does',
         tasks: [
             {
+                id: 'sound',
                 title: 'Can you make him go back for a second-helping (but hold the salt)?',
                 description: 'Which sounds saltier to you?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'low',
                         title: 'Pump up the bass: fill the room with low background music',
                         response: 'Correct! Studies show that playing low-pitched music increases the perceived umami-ness of a meal - do that, and you reduce the need for additional salt in food.',
                         isCorrect: true
                     },
                     {
-                        id: 'light',
+                        id: 'loud',
                         title: 'Pump up the volume: fill the room with loud background music',
                         response: 'Sorry! This won\'t do anything except annoy your patrons - there\'s a difference between great atmosphere and not being able to hear each other.',
                         isCorrect: false
@@ -105,17 +110,18 @@ const CHALLENGES = {
                 ]
             },
             {
+                id: 'colour',
                 title: 'Can you make him go back for a second-helping (but hold the salt)?',
                 description: 'Just desserts: which colour scheme boosts the appetite?',
                 options: [
                     {
-                        id: 'heavy',
+                        id: 'multi',
                         title: 'Serve a multi-coloured pudding',
                         response: 'Correct! Foods that come in multiple colours trick the brain so you eat more than you would if it came in a single colour (even if it is their favourite).',
                         isCorrect: true
                     },
                     {
-                        id: 'light',
+                        id: 'mono',
                         title: 'Stick to a single palate - what’s their favourite colour?',
                         response: 'Sorry! This was a bit of a trick: multicoloured foods will make a person more susceptible to over-indulging - even more than a whole dish prepared in their favourite colour.',
                         isCorrect: false
