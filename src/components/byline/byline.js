151a193b27c4e1e96bd62ae1070e2577fdcca2c0
Saul Hardman <hello@iamsaul.co.uk>
2016-05-21 18:08:37 +0100

    feat(Share): set default events

--- src/components/byline/byline.js
+++ src/components/byline/byline.js
@@ -1,4 +1,3 @@
-import events from '../../js/modules/events';
 import template from './byline.html!text';
 
 // Components
@@ -9,15 +8,6 @@ let Byline = Block.extend({
     template,
     components: {
         Share
-    },
-    data() {
-        return {
-            shareEvents: {
-                SHARE_FACEBOOK: events.SHARE_FACEBOOK,
-                SHARE_TWITTER: events.SHARE_TWITTER,
-                SHARE_EMAIL: events.SHARE_FACEBOOK
-            }
-        };
     }
 });
 
--- src/components/share/share.js
+++ src/components/share/share.js
@@ -1,14 +1,20 @@
 import Vue from 'vue';
 
+import events from '../../js/modules/events';
 import template from './share.html!text';
 
 let Share = Vue.extend({
     template,
-    props: [
-        'text',
-        'url',
-        'events'
-    ]
+    props: {
+        text: String,
+        url: String,
+        events: {
+            type: Object,
+            default() {
+                return events;
+            }
+        }
+    }
 });
 
 export default Share;
