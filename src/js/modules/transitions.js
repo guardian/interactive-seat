3ec22e6e33b6e809680ff8c87122fe405cdf20fe
Saul Hardman <hello@iamsaul.co.uk>
2016-05-23 02:21:01 +0100

    feat(Animations): improve animation appearance
    - use `before` and `after` animation hooks to add `enter` and `leave` classes
    - prevent flash before animations on iOS

--- src/js/modules/transitions.js
+++ src/js/modules/transitions.js
@@ -3,38 +3,40 @@ import Vue from 'vue';
 import { cssAnimationEndEventName } from '../../js/lib/browser';
 import { supportsCssAnimation } from '../../js/lib/support';
 
-let transition = function (className) {
-    return function (el, done) {
-        // guard against lack of CSS animation support
-        if (!supportsCssAnimation) {
-            done();
-
-            return;
-        }
-
-        let promises = [].map.call(el.querySelectorAll('.animate'), (el) => {
-            let promise = new Promise((resolve) => {
-                let eventHandler = () => {
-                    resolve();
-
-                    el.classList.remove(className);
-
-                    el.removeEventListener(cssAnimationEndEventName, eventHandler, false);
-                };
-
-                el.addEventListener(cssAnimationEndEventName, eventHandler, false);
-            });
-
+let addClass = function (className) {
+    return function (el) {
+        [].forEach.call(el.querySelectorAll('.animate'), (el) => {
             el.classList.add(className);
-
-            return promise;
         });
-
-        Promise.all(promises).then(done);
     };
 };
 
-let cancelTransition = function (className) {
+let transition = function (el, done) {
+    // guard against lack of CSS animation support
+    if (!supportsCssAnimation) {
+        done();
+
+        return;
+    }
+
+    let promises = [].map.call(el.querySelectorAll('.animate'), (el) => {
+        let promise = new Promise((resolve) => {
+            let eventHandler = () => {
+                resolve(el);
+
+                el.removeEventListener(cssAnimationEndEventName, eventHandler, false);
+            };
+
+            el.addEventListener(cssAnimationEndEventName, eventHandler, false);
+        });
+
+        return promise;
+    });
+
+    Promise.all(promises).then(done);
+};
+
+let removeClass = function (className) {
     return function (el) {
         [].forEach.call(el.querySelectorAll('.animate'), (el) => {
             el.classList.remove(className);
@@ -44,8 +46,12 @@ let cancelTransition = function (className) {
 
 Vue.transition('interactive-view', {
     css: false,
-    enter: transition('enter'),
-    enterCancelled: cancelTransition('enter'),
-    leave: transition('leave'),
-    leaveCancelled: cancelTransition('leave')
+    beforeEnter: addClass('enter'),
+    enter: transition,
+    afterEnter: removeClass('enter'),
+    enterCancelled: removeClass('enter'),
+    beforeLeave: addClass('leave'),
+    leave: transition,
+    afterLeave: removeClass('leave'),
+    leaveCancelled: removeClass('leave')
 });
